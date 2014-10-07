410d10ef04d6110f0a7ce3c0f42e0c29d3b550a4
Cameron Roe <cameron.b.roe@gmail.com>
2014-10-07 13:28:33 -0500

    Removed binary file

--- main.c
+++ main.c
@@ -4,12 +4,21 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <signal.h>
 
 // Local includes
 #include "env.h"
 #include "input.h"
 
 shell_env env;
+
+void ignore_sigint() {
+	signal(SIGINT, SIG_IGN);
+}
+
+void ignore_sigtstp() {
+	signal(SIGTSTP, SIG_IGN);
+}
 
 void shell_exec(char* command, char** argv) {
 	pid_t pid;
@@ -32,8 +41,18 @@ int main(int argc, int argv) {
 	system("clear"); // Clear the terminal
 	load_env(&env, NULL);
 	char* input = malloc(1024 * sizeof(char)); // Allocate a kilobyte of memory for the input for the shell, this should be sufficient for commands
+
+	// Handle some basic signals
+	struct sigaction handler_sigint;
+	handler_sigint.sa_handler = ignore_sigint;
+	sigaction(SIGINT, &handler_sigint, NULL);
+
+	struct sigaction handler_sigtstp;
+	handler_sigtstp.sa_handler = ignore_sigtstp;
+	sigaction(SIGTSTP, &handler_sigtstp, NULL);
+
 	do {
-		printf("%s%s:   ", env.prompt, env.pwd);
+		printf("%s%s: ", env.prompt, env.pwd);
 		gets(input);
 		char* split = " ";
 		char **arr = malloc(1024 * 1024);
